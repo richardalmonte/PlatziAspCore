bd985607c3df22329dd9815820bfbe7781e01c14
Richard Almonte <richard_almonte@hotmail.com>
2019-04-11 00:01:48 +0200

    Passing data using ViewBag and ViewData Changing default layout

--- Controllers/SchoolController.cs
+++ Controllers/SchoolController.cs
@@ -15,7 +15,7 @@ namespace PlatziAspCore.Controllers
             school.FoundationYear = 2005;
             school.SchoolId = Guid.NewGuid().ToString();
             school.Name = "Platzi School";
-            
+            ViewBag.DynamicData= "Test Text";
             return View(school);
         }
         #endregion
